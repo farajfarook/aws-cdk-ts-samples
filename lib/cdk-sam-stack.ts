1a42b07476e1b7bd028eea1a59c6af6e9e679d2a
Faraj Farook <farajfarook@gmail.com>
2019-09-01 20:24:00 +1000

    Adding code

--- lib/cdk-sam-stack.ts
+++ lib/cdk-sam-stack.ts
@@ -1,9 +1,42 @@
-import cdk = require('@aws-cdk/core');
+import { Stack, Construct, StackProps } from "@aws-cdk/core"
+import { Bucket } from "@aws-cdk/aws-s3"
+import * as apigw from "@aws-cdk/aws-apigateway"
+import * as lambda from "@aws-cdk/aws-lambda"
+import * as path from "path"
+import * as iam from "@aws-cdk/aws-iam"
 
-export class CdkSamStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class CdkSamStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        let bkt = Bucket.fromBucketName(this, "tyst-buck", "tyst-buck-xx");
+
+        if (bkt == null)
+            new Bucket(this, "tyst-buck", {
+                versioned: false,
+                bucketName: "tyst-buck-xx"
+            })
+
+        let api = new apigw.RestApi(this, "test-api123-xx", {
+            restApiName: "Test API 123",
+            deployOptions: {
+                stageName: "v1"
+            }
+        })
+
+        api.root.addMethod('ANY')
+
+        let apisam = api.root.addResource("apisam")
+
+        let apisam_lambda = new lambda.Function(this, "apisam-lambda", {
+            code: lambda.Code.fromAsset(path.join(__dirname, '..', 'archive.zip')),
+            handler: 'ApiSam::ApiSam.LambdaEntryPoint::FunctionHandlerAsync',
+            runtime: lambda.Runtime.DOTNET_CORE_2_1,
+            role: iam.Role.fromRoleArn(this, "lambda-basic", "arn:aws:iam::xxx:role/lambda-basic")
+        });
+
+        let apisamproxy = apisam.addProxy({
+            defaultIntegration: new apigw.LambdaIntegration(apisam_lambda),
+        })
+    }
 }
